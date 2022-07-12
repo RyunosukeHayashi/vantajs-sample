74cd93711dec19af5110f2f38bfc813d2c5d5225
Hayashi Ryunosuke <ryu@Hayashis-MacBook-Air.local>
2022-07-12 09:13:01 +0900

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const threeScript=document.createElement('script')
+    threeScript.setAttribute('id','threeScript')
+    threeScript.setAttribute('src','three.r119.min.js')
+    threeScript.setAttribute('src','vanta.net.min.js')
+    document.getElementsByTagName('head')[0].appendChild(threeScript)
+    return()=>{
+      if(threeScript){
+        threeScript.remove
+      }
+    }
+  }, [])
+  
   return <Component {...pageProps} />
 }
 
